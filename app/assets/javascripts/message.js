726250d41bf25be02fad9454e0e033343459bdae
toshihi07 <55124827+toshihi07@users.noreply.github.com>
2019-09-25 18:55:53 +0900

    自動更新機能

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,30 +1,30 @@
 $(document).on('turbolinks:load', function() {
+  
   function buildHTML(message){
-    var MessageImage = message.image ? `<img src="${message.image}" class="lower-message__image">` : ``;
-    var html = `
-        <div class="message" data-message_id="${message.id}">
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${ message.user_name }
-            </div>
-            <div class="upper-message__date">
-              ${ message.time }
-            </div>
-          </div>
-          <div class="lower-meesage">
-            <p class="lower-message__content">
-              ${ message.content }
-            </p>
-            ${ MessageImage }
-          </div>
-        </div>`;
+    var MessageImage = message.image? `<img src="${message.image}" class="lower-message__image">` : ``;
+    var html = `<div class="message" data-message_id="${message.id}">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${ message.user_name }
+                    </div>
+                    <div class="upper-message__date">
+                      ${ message.time }
+                    </div>
+                  </div>
+                  <div class="lower-meesage">
+                    <p class="lower-message__content">
+                      ${ message.content }
+                    </p>
+                    ${ MessageImage }
+                  </div>
+                </div>`;
     return html;
-}
-
+  }
+  
   function Scroll(){
     $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
   }
-
+  
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -48,4 +48,33 @@ $(document).on('turbolinks:load', function() {
       alert('メッセージの送信に失敗しました');
     })
   })
+  let reloadMessages = function() {                                 //（正規表現： \で直後の記号文字を毎回エスケープ）
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){   // ifでグループメッセージの画面でのみ自動更新
+      let last_message_id = $('.message').last().data('message_id');
+      console.log(last_message_id)
+
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0){               //メッセージの中身があればappend&スクロールするようにする
+      let insertHTML = '';                      //追加するHTMLの入れ物を作る
+      
+      messages.forEach(function(message) {      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        insertHTML = buildHTML(message);        //メッセージが入ったHTMLを取得
+        $('.messages').append(insertHTML);      //メッセージを追加
+      })
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast')   //メッセージ分だけスクロール
+      }
+    })
+
+    .fail(function() {
+      alert('自動更新に失敗しました');
+    });
+  }
+};
+  setInterval(reloadMessages, 5000);
 });
